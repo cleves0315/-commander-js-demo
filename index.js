7b162032d406add61da5db440642412b30c3d234
cleves0315 <cleves0315@foxmail.com>
2022-03-12 17:32:41 +0800

    feat: custom command and agument

--- index.js
+++ index.js
@@ -9,11 +9,30 @@ function collect(value, previous) {
   return [...previous, value]
 }
 
+function myParseInt(value) {
+  return Number(value)
+}
+
 program
   .option('-d, --debug', 'output extra debugging')
   .option('-de, --default', 'default value', defaultValue)
   .option('-c, --cheese [type]', 'add the specified type of cheese')
   .option('-co, --collect <n>', 'small pizza size', collect, []);
+
+program
+  .command('clone <source> [destination]')
+  .description('clone a repository into a newly created directory')
+  .action((source, destination) => {
+    console.log(`clone command called: ${source} to ${destination}`);
+  })
+
+program
+  .command('add')
+  .argument('<first>', 'number argument', myParseInt)
+  .argument('[second]', 'number argument', myParseInt, 10)
+  .action((first, second) => {
+    console.log(`first: ${first}, second: ${second}: ${first + second}`)
+  })
 
 program.version('0.0.1', '-v --vers', 'output the current version');
 // process.argv
